80c306beed0db1145c20f0f007e74b3b4456ba7c
PrashantMantri24 <mantri_prashant@yahoo.co.in>
2023-04-26 12:13:54 +0530

    input output prcessing done

--- app.js
+++ app.js
@@ -7,12 +7,13 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output")
+
+// console.log(txtInput);
 
 
 function clickHandler() {
-    console.log("clicked");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "fffhhhhjhj" + txtInput.value 
 }
 
 btnTranslate.addEventListener("click", clickHandler);
